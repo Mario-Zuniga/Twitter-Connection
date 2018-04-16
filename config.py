8cfbfae0a70c5f1663b3635dd10cdb411c5e02a6
Mario-Zuniga <30329465+Mario-Zuniga@users.noreply.github.com>
2018-04-15 21:17:32 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #   https://apps.twitter.com/app/new
 
-con_secret = "4341656860-tzWR2CvFMV5tSI0UQimXLKJab01KSsvmDYkge1Y"
-con_secret_key = "aZ82mJzoW7uP7dsol7DSIV5AbMNFINJHDEhNBK2qUFnE3"
-token = "woSvH8DZF6q5X68CcYslwNlAh"
-token_key = "3fNIFWVVMRBhApmeUeAo4OhyzLHu6i3zlgbkEOuk1b2oGMYBLd"
+con_secret = "con_secret goes here"
+con_secret_key = "con_secret_key goes here"
+token = "token goes here"
+token_key = "token_key goes here"
